94bb69f38b0289406bf5162faeb2ef6ac5e3b4eb
shapran_roman <romanshapran96@gmail.com>
2020-12-23 18:00:53 +0300

    add Dockerfile, docker-compose, nginx, postgres and uwsgi

--- app/blogs/settings.py
+++ app/blogs/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
@@ -78,11 +78,21 @@ WSGI_APPLICATION = 'blogs.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogs',
+        'USER': 'root',
+        'PORT': 5432,
     }
 }
 
@@ -124,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DOMAIN_NAME = 'http://127.0.0.1'
 PORT = 8000
